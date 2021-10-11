c803f85a28dee051533376bb7cb308673b8ef1eb
Vladimir Kovalev <boban_envorter@mail.ru>
2021-10-11 19:40:36 +0300

    editing

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 500;
         int milesPrice = 20;
-        int bonusMiles = ticketPrice/milesPrice;
+        int bonusMiles = ticketPrice / milesPrice;
         System.out.println(bonusMiles);
     }
 }
